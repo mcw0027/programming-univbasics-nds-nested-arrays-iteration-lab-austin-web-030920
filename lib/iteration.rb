83eff96ed2b96dbcbbd17ae3138793fb6be8e7b9
Michael Wixson <wixsonmc@gmail.com>
2020-02-06 17:05:18 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -11,8 +11,10 @@ def join_ingredients(src)
   while row_index < array_1.count do
     element_index = 0
     while element_index < array_1[row_index].count do
-      puts "I love" + '' + array_1[row_index][0] + '' + "and" + '' + array_1[row_index][1] + '' + on my pizza
-  
+      puts "I love" + '' + array_1[row_index][0] + '' + "and" + '' + array_1[row_index][1] + '' + "on my pizza"
+      element_index += 1
+  end
+  row_index += 1
 end
 
 def find_greater_pair(src)
