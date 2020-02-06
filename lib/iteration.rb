4472543e316a63c5d0b633b339fd861dc7442eb7
Michael Wixson <wixsonmc@gmail.com>
2020-02-06 17:07:38 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -12,7 +12,7 @@ def join_ingredients(src)
   while row_index < array_1.count do
     element_index = 0
     while element_index < array_1[row_index].count do
-      puts "I love" + '' + array_1[row_index][0] + '' + "and" + '' + array_1[row_index][1] + '' + "on my pizza"
+      return "I love" + '' + array_1[row_index][0] + '' + "and" + '' + array_1[row_index][1] + '' + "on my pizza"
       element_index += 1
   end
   row_index += 1
